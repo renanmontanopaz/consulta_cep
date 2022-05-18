5c8f4bf2cab7a1cf0f6102c8bc642fa8ddc1522a
renan <renanmontanopaz@outlook.com>
2022-05-18 19:02:27 -0300

    formulário finalizado !

--- script.js
+++ script.js
@@ -5,29 +5,44 @@ let cidade = document.querySelector('#cidade');
 let UF = document.querySelector('#uf');
 let ibge = document.querySelector('#ibge');
 let ddd = document.querySelector('#ddd');
+let tempo = ''
 /*cep.value = '0';*/
-cep.addEventListener('blur', function(e){
+function delay(n){
+    return new Promise(function(resolve){
+        rua.value = 'carregando...'
+        bairro.value = 'carregando...'
+        UF.value = '...'
+        ibge.value = 'carregando...'
+        ddd.value = '...'
+        cidade.value = 'carregando...'
+        setTimeout(resolve,n*1000);
+    });
+}
+cep.addEventListener('blur', async function(e){
     let cep = e.target.value;
     let script = document.createElement('script');
     const small = document.getElementById('small');
     const Cep = document.getElementById('cep');
+    const inputis = document.querySelectorAll("input")
     if(Cep.value === '') {
         Cep.style.borderColor = "red"
         small.innerHTML = "Esse campo é obrigatório!"
     }
     script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=callbackform';
     document.body.appendChild(script);
-})(1000);
+});
 
-function callbackform(resposta){
+async function callbackform(resposta){
     const small = document.querySelector("small");
     const inputs = document.querySelectorAll("input")
+    small.innerHTML = ""
     if("erro" in resposta){
         cep.style.borderColor = "red"
         small.innerHTML = "CEP inválido!"
+        inputs.value = ""
     }else {
-        small.innerHTML = ""
         cep.style.borderColor = "green"
+        await delay(2);
         rua.value = resposta.logradouro;
         bairro.value = resposta.bairro;
         cidade.value = resposta.localidade;
