da9758f4159efd693a9883818fe4c34c10b1701b
renan <renanmontanopaz@outlook.com>
2022-05-18 17:01:13 -0300

    script atualizado

--- script.js
+++ script.js
@@ -5,23 +5,34 @@ let cidade = document.querySelector('#cidade');
 let UF = document.querySelector('#uf');
 let ibge = document.querySelector('#ibge');
 let ddd = document.querySelector('#ddd');
-cep.value = '01001000';
+/*cep.value = '0';*/
 cep.addEventListener('blur', function(e){
     let cep = e.target.value;
     let script = document.createElement('script');
+    const small = document.getElementById('small');
+    const Cep = document.getElementById('cep');
+    if(Cep.value === '') {
+        Cep.style.borderColor = "red"
+        small.innerHTML = "Esse campo é obrigatório!"
+    }
     script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=callbackform';
     document.body.appendChild(script);
-});
+})(1000);
 
 function callbackform(resposta){
-
+    const small = document.querySelector("small");
+    const inputs = document.querySelectorAll("input")
     if("erro" in resposta){
-
+        cep.style.borderColor = "red"
+        small.innerHTML = "CEP inválido!"
+    }else {
+        small.innerHTML = ""
+        cep.style.borderColor = "green"
+        rua.value = resposta.logradouro;
+        bairro.value = resposta.bairro;
+        cidade.value = resposta.localidade;
+        UF.value = resposta.uf;
+        ibge.value = resposta.ibge;
+        ddd.value = resposta.ddd;
     }
-    rua.value = resposta.logradouro;
-    bairro.value = resposta.bairro;
-    cidade.value = resposta.localidade;
-    UF.value = resposta.uf;
-    ibge.value = resposta.ibge;
-    ddd.value = resposta.ddd;
 }
